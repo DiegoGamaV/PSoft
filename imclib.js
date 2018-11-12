ba2547176d218625e9644958c10d5f90de54e2e9
Diego A. Gama <diego.gama@ccc.ufcg.edu.br>
2018-11-12 15:06:06 -0300

    modularized scripts with module pattern

---
+++ imclib.js
@@ -0,0 +1,44 @@
+const imclib = (function(){
+    
+    gerarClassificacao = function(valor) {
+        if (valor < 17) {
+            return "Muito abaixo do peso";
+        } else {
+            if (valor < 18.49) {
+                return "Abaixo do peso";
+            } else {
+                if (valor < 24.99) {
+                    return "Peso normal";
+                } else {
+                    if (valor < 29.99) {
+                        return "Acima do peso";
+                    } else {
+                        if (valor < 34.99) {
+                            return "Obesidade I";
+                        } else {
+                            if (valor < 39.99) {
+                                return "Obesidade II (servera)";
+                            } else {
+                                if (valor >= 40)
+                                    return "Obesidade III (mórbida)";
+                                else
+                                    return "";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    calcularImc = function(altura, massa){
+        return massa / (altura * altura);
+    };
+
+    var object = {
+        classificacao : gerarClassificacao,
+        imc : calcularImc
+    };
+
+    return object;
+})();
