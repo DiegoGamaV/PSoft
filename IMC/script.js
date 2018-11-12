d6fa60546cf7478db1daab70975d4fc076ee52c3
Diego A. Gama <diego.gama@ccc.ufcg.edu.br>
2018-11-12 11:35:38 -0300

    Added IMC files

---
+++ IMC/script.js
@@ -0,0 +1,14 @@
+const altura_field = document.getElementById("altura");
+const massa_field = document.getElementById("massa");
+const outputValor = document.getElementById("show");
+const outputClass = document.getElementById("classificacao");
+
+function exibirIMC(){
+    var altura = Number(altura_field.value);
+    var massa = Number(massa_field.value);
+
+    var resultado = imclib.imc(altura, massa).toPrecision(2);
+    outputValor.innerHTML = "IMC: " + resultado;
+    if (resultado > 0)
+        outputClass.innerHTML = imclib.classificacao(resultado);
+}
